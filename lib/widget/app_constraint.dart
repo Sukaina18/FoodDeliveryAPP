3b774f3931fc8fba52a6f0138544947f686e65f2
Sukaina18 <121991365+Sukaina18@users.noreply.github.com>
2024-06-20 13:28:13 +0530

    Update app_constraint.dart

--- lib/widget/app_constraint.dart
+++ lib/widget/app_constraint.dart
@@ -1,3 +1,1 @@
 
-String publishableKey = "pk_test_51PQNu0P2cqGXp4XEmkqgOVwxe86vdthCoL7M1vdfu18jvhlNzkDR4VA6JjUPyXLariyEhQxo5cZQMuZ99eMNIjQw00SdVGyDRJ";
-String secretKey = "sk_test_51PQNu0P2cqGXp4XEdsxXDD2Px8QJowqAmXBq98DqJtWPcy2DjCklPtuXHN2RJF9ALlPCfWyXdfO2XhdNgsypCu2m000E86nFyY";
